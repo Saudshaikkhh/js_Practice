0d70d0b1b1c5ec298928adc9e81e14b240f3229e
Saud <Shaikhmohdsaud2004@gmail.com>
2025-01-05 16:35:43 +0530

    file explorer
    just addedd new file

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,27 @@
+let num1 = 8;   
+let negnum = -num1;
+console.log(negnum); // output-8
+
+//Mathematical Operations
+console.log(2+2); //Output: 4
+console.log(2-2); //Output: 0
+console.log(2*2); //Output: 4
+console.log(2**2);//Output: 4
+console.log(2/3); //Output: 0.6666666666666666
+console.log(2%2); //Output: 0
+console.log(2);   //Output: 2
+console.log(+2);  //Output: 2
+console.log(+true) //Output: 1
+console.log(+" ") //Output: 0
+
+//Research study Concatenation
+console.log(2 + "1") //Output: 21
+console.log("2" + 1) //Output: 21
+console.log("2" + 1 + 5 ) //Output: 215
+console.log(2 + 1 + "5" ) //Output: 35 At starting addition Operation is working.
+
+//Increment and decrement inshort
+let gamescore = 100
+gamescore++
+console.log(gamescore); //Output: 101
+
