46b16dd8618e426b322c594d099a3740cf81304f
Saud <Shaikhmohdsaud2004@gmail.com>
2025-01-12 14:33:40 +0000

    Scopes updated and added

--- 02_Basics/04_Scope.js
+++ 02_Basics/04_Scope.js
@@ -27,5 +27,57 @@ if (true) {
 console.log(c); // 60 this should not happen
 console.log(a); //Output: 20
 
+//Scope level 
 
+// function one() {
+//     const Name = "Shaikh Mohammed Saud"
+//     function two() {
+//         const website = "youtube"
+//         console.log(Name);
+//     }
+//     console.log(website);
+//     two()
+    
+// }
+// One()
+/*
+Note:
+The above gave error beacause
+In simple terms, the error happens because the website variable is defined inside the two() function, 
+so it can only be used inside two(). When you try to access website outside of two() (in one()), 
+it's not found, resulting in an error. However, Name is accessible inside two() because it's in the 
+outer function (one()), so it works fine there
+*/
+ 
+// Same thing goes for if blocks it will gives error
+// if (true) {
+//     const username = "saud shaikh"
+//     if(username === "saud shaikh"){
+//         const website = "ultrainstinct.com"
+//         console.log(website+username)
+//     }
+//     console.log(website);
+    
+// }
+// console.log(username);
 
+//Mini hosting
+
+console.log(addone(45));
+function addone(num1) {
+    return num1 +1
+    
+}
+
+/*
+Note:
+Mini hoisting refers to JavaScript's behavior of moving variable and function declarations to the top
+of their scope before executing the code. However, only the declaration is hoisted, not the value assignment. 
+For example, if you try to use a variable before it's assigned a value, it will return undefined because only 
+the declaration was hoisted, not the value.
+*/
+//console.log(newfun(55)); //Unlike above declaration of function it will give error
+const newfun = function (num1) {
+    return num1 + 2
+}
+//console.log(newfun(55)); Output: 57
