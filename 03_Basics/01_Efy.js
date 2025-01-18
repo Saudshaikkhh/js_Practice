5928d3d5dc247e94de127f97dfb672b94ca7a9da
Saud <Shaikhmohdsaud2004@gmail.com>
2025-01-18 09:43:58 +0000

    new

--- 03_Basics/01_Efy.js
+++ 03_Basics/01_Efy.js
@@ -1,9 +1,11 @@
 //Immediately Invoked Function Expressions (IIFE).
 
+//Named eefi
 (function () {
     console.log(`Shaikh Mohammed Saud`);
 })();
 
+//Unamed eefi or simple eefi
 ( () =>{
     console.log(`Shaikh anwar`);
     
